f48696959d370374a92aff7a560e19dc687f8697
Tanmayjain0906 <tanmayjain983@gmail.com>
2025-04-10 08:20:31 +0530

    reviewer

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const aiRoute = require("./routes/ai.route");
+const cors = require("cors");
 
 const app = express();
+
+const aiRoute = require("./routes/ai.route");
+
+app.use(cors({
+    origin: "https://code-reviewer-frontend-seven.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use("/ai", aiRoute);
